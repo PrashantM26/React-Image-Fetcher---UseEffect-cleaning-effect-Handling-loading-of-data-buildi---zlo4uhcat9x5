7ddf29ec3e73a155cb61b3902db575fa813fce57
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-09-25 20:03:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import './styles/App.css'; // Link to your CSS file
-import Loader from './Loader';
-import PhotoFrame from './PhotoFrame';
-
-function App() {
-  const [inputNumber, setInputNumber] = useState('');
-  const [photoData, setPhotoData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    // Function to fetch data from the API
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${inputNumber}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setPhotoData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-
-      setIsLoading(false);
-    };
-
-    if (inputNumber !== '') {
-      fetchData();
-    }
-  }, [inputNumber]);
-
-  const handleInputChange = (e) => {
-    setInputNumber(e.target.value);
-  };
-
+import React from 'react'
+import '../styles/App.css';
+import { Loader } from './Loader';
+import { PhotoFrame } from './PhotoFrame';
+const App = () => {
+  const[id,setId] = React.useState(null);
+  const[photo,setPhoto] = React.useState(null);
+  console.log(photo)
+  React.useEffect(()=>{
+    if(id === 0) return;
+    // console.log("Hi Alok")
+      setPhoto(null)
+      fetch(`https://jsonplaceholder.typicode.com/photos/${id}`).then((response)=>{
+        return response.json()
+      }).then(result=>{
+        setPhoto(result)
+      })
+  },[id])
   return (
     <div className="App">
-      <label>Id Number </label>
-      <input
-        type="number"
-        placeholder="Enter a number between 1-5000"
-        value={inputNumber}
-        onChange={handleInputChange}
-      />
-      {isLoading && <Loader />}
-      {!isLoading && photoData && (
-        <PhotoFrame url={photoData.url} title={photoData.title} />
-      )}
+      <input type="number" onChange={(event)=>{
+        setId(event.target.value)
+      }}/>
+      {photo && <PhotoFrame title={photo.title} url={photo.url} />}
+      {id && !photo && <Loader/>}
     </div>
   );
 }
+
 
 export default App;
