7f105ee93fa5d81495b0aa40edf1fa992d7c6fb8
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-09-25 20:10:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {createRoot} from "react-dom/client"
-import App from "./components/App.js"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-createRoot(
-  document.getElementById("root")
-).render(<App />)
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
